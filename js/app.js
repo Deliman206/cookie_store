ae75ac376a058ae02f35724a74a244042eb13ea3
Sean Miller <smiller206@me.com>
2018-02-12 19:07:12 -0800

    Finished Store 2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 'use strict';
+
+//Try to remember to do a table for results. Box in sloppy.
+
 // with for loop where i length is hours
 // we get diff random numbers in each hour
 // we also get diff # cookies sold per hour
 // thus cookie total is the sum of many elements in an array
-
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+//Cookie Stand Alki
 var alkiCookieStand = {
     name: 'Alki',
     minCust: 2,
@@ -13,33 +17,103 @@ var alkiCookieStand = {
     custDay: [],
     cookieSoldDay: [],
     custHour: function(){
-        for (var i = 0; i< this.storeHours; i++){
+        for (var i = 0; i<=this.storeHours; i++){
             this.custDay.push(Math.floor(Math.random()*(this.maxCust-this.minCust)+this.minCust));
             console.log(this.custDay);
         }
     },
     cookieSoldHour: function(){
-        for(var i=0; i<this.storeHours; i++){
+        for(var i=0; i<=this.storeHours; i++){
             this.cookieSoldDay.push(Math.ceil(this.avgCookieCust * this.custDay[i]));
             console.log(this.cookieSoldDay);
         }
     },
     render: function(){
-        var ulEl = document.getElementById('alkiStore');
         this.custHour();
-        for (var i=0; i< this.custDay.length; i++){
+        this.cookieSoldHour();
+        var ulEl = document.getElementById('alkiStore');
+        for (var i=0; i<=this.storeHours; i++){
             //create element
             var liEl = document.createElement('li');
             //give it content
-            liEl.textContent = this.custDay[i];
+            liEl.textContent = hours[i] + ': '+ this.cookieSoldDay[i];
             //append to DOM
             //parent.appendChild(child)
             ulEl.appendChild(liEl);
         }
-        
-        this.cookieSoldHour();
     }
 };
-// alkiCookieStand.custHour();
-// alkiCookieStand.cookieSoldHour();
+//Cookie Stand Pike
+var pikeCookieStand = {
+    name: 'Pike',
+    minCust: 23,
+    maxCust: 65,
+    avgCookieCust: 6.3,
+    storeHours: 14,
+    custDay: [],
+    cookieSoldDay: [],
+    custHour: function(){
+        for (var i = 0; i<=this.storeHours; i++){
+            this.custDay.push(Math.floor(Math.random()*(this.maxCust-this.minCust)+this.minCust));
+            console.log(this.custDay);
+        }
+    },
+    cookieSoldHour: function(){
+        for(var i=0; i<=this.storeHours; i++){
+            this.cookieSoldDay.push(Math.ceil(this.avgCookieCust * this.custDay[i]));
+            console.log(this.cookieSoldDay);
+        }
+    },
+    render: function(){
+        this.custHour();
+        this.cookieSoldHour();
+    var ulEl = document.getElementById('pikeStore');
+    for (var i=0; i<=this.storeHours; i++){
+        //create element
+        var liEl = document.createElement('li');
+        //give it content
+        liEl.textContent = hours[i] + ': '+ this.cookieSoldDay[i];
+        //append to DOM
+        //parent.appendChild(child)
+        ulEl.appendChild(liEl);
+    }
+}
+};
+//Cookie Stand Pike
+// var seaTacCookieStand = {
+//     name: 'SeaTac',
+//     minCust: 3,
+//     maxCust: 24,
+//     avgCookieCust: 1.2,
+//     storeHours: 14,
+//     custDay: [],
+//     cookieSoldDay: [],
+//     custHour: function(){
+//         for (var i = 0; i<=this.storeHours; i++){
+//             this.custDay.push(Math.floor(Math.random()*(this.maxCust-this.minCust)+this.minCust));
+//             console.log(this.custDay);
+//         }
+//     },
+//     cookieSoldHour: function(){
+//         for(var i=0; i<=this.storeHours; i++){
+//             this.cookieSoldDay.push(Math.ceil(this.avgCookieCust * this.custDay[i]));
+//             console.log(this.cookieSoldDay);
+//         }
+//     },
+//     render: function(){
+//         this.custHour();
+//         this.cookieSoldHour();
+//         var ulEl = document.getElementById('seatacStore');
+//         for (var i=0; i<=this.storeHours; i++){
+//             //create element
+//             var liEl = document.createElement('li');
+//             //give it content
+//             liEl.textContent = hours[i] + ': '+ this.cookieSoldDay[i];
+//             //append to DOM
+//             //parent.appendChild(child)
+//             ulEl.appendChild(liEl);
+//         }
+//     }
+// };
 alkiCookieStand.render();
+pikeCookieStand.render();
